2d082c44e1971a33da1658113f104ea7ff7b6743
jj88zz <jj88zz@outlook.com>
2019-01-26 12:32:17 +0800

    引用

--- get-sub.sh
+++ get-sub.sh
@@ -3,3 +3,6 @@ git submodule add https://github.com/z4-okj8/jupyterlab.git
 git submodule add https://github.com/z4-okj8/binderhub.git
 
 git submodule update
+
+
+git submodule add
--- src/setup-jupyterlab.sh
+++ src/setup-jupyterlab.sh
@@ -1,5 +1,10 @@
 conda install -c conda-forge jupyterlab
 
+echo "安装node,java脚本的支持"
 conda install -c conda-forge nodejs
 
+echo "安装文档格式的支持"
+conda install -c conda-forge jupytext
+
+echo "安装多用户协作支持"
 jupyter serverextension enable --py jupyterlab --sys-prefix
