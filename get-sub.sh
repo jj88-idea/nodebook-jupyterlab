6b0585d1c3d9d1e1c12a846fec698dd8c30e3dc3
jj88zz <jj88zz@outlook.com>
2019-01-26 13:43:32 +0800

    引用

--- get-sub.sh
+++ get-sub.sh
@@ -7,8 +7,9 @@ git submodule add https://github.com/z4-okj8/binderhub.git
 
 #有用的ext
 git submodule add https://github.com/z4-okj8/awesome-jupyterlab.git /doc/jupyterlab-ext
-
 git submodule add https://github.com/z4-okj8/awesome-jupyter.git  /doc/jupyter-ext
 
 
 git submodule update
+
+git submodule sync
